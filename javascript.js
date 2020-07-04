110345c893332a1a0fbdb48d26476d427f4c551b
Josh Nice <josh.nice@iventis.co.uk>
2020-07-04 12:23:58 +0100

    added base site and first example of side menu

---
+++ javascript.js
@@ -0,0 +1,66 @@
+window.onload = async function() {
+
+    console.log('loading is complete');
+    const window_height = window.innerHeight;
+    const menu_text = insertingMenuElements();
+
+    for (let i = 0; i < menu_text.length; i++) {
+        $('#' + menu_text[i]).hide();
+    }
+
+    const side_bar_menu_animations = anime({
+        targets: '#basic-box',
+        borderRadius: ['50%', '0%'],
+        easing: 'easeInOutQuad',
+        width: '300px',
+        height: ['100px', window_height / 4],
+        duration: 3000,
+        autoplay: false,
+        complete: function() {
+            animateMenuText(menu_text, 0);
+        }
+    });
+
+
+
+    $( "#basic-box" ).on( "click", async  function() {
+        await side_bar_menu_animations.play();
+    });
+    
+
+    
+
+};
+
+function insertingMenuElements() {
+    const menu_text_elements = document.getElementsByClassName("menu-text");
+    const menu_text_ids = [];
+
+    for (let i = 0; i < menu_text_elements.length; i++) {
+        menu_text_ids.push(menu_text_elements[i].id);
+    }
+
+    console.log('menu text ids', menu_text_ids);
+    return menu_text_ids;
+}
+
+function animateMenuText(menu_text, index) {
+
+    if (menu_text[index] == null) {
+        return;
+    }
+
+    $('#' + menu_text[index]).show();
+
+    const side_bar_text_animaition = anime({    
+        targets: '#' + menu_text[index],
+        translateX: 100,
+        delay: anime.stagger(100),
+        complete: function() {
+            index++;
+            animateMenuText(menu_text, index);
+        }
+    });
+
+    side_bar_text_animaition.play();
+}
